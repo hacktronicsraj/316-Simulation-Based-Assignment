9233f7cb023922a393f88c72260d30d526b2c6a2
hacktronicsraj <69853022+hacktronicsraj@users.noreply.github.com>
2023-03-06 01:31:14 +0530

    Add files via upload
    takes input and stores them in their respective queue according to their priority

---
+++ Simulation.py
@@ -0,0 +1,21 @@
+#This is a part of the whole code and only has input taking and printing out the queues , I need to work on how to take the queues and working on them on how to put them in an cpu scheduling and algorithm and also I have to switch between queues of different algorithms every 10 seconds
+ 
+queue1 = []  # high priority range (0-3) for Round Robin q t 4
+queue2 = []  # medium priority range (4-6) for priority scheduling algorithm
+queue3 = []  # lowest priority range (7-9) for First come first serve algorithm
+ 
+# Taking user input from the user of process id and priority and burst time
+num_processes = int(input("Enter the num of processes: "))
+for i in range(num_processes):
+    priority = int(input("Enter the priority of process {}: ".format(i+1)))
+    burst_time = int(input("Enter the burst time of process {}: ".format(i+1)))
+    process = [i+1, priority, burst_time]  # Process id , and priority and birst time are stored in this order.
+    if priority >= 0 and priority <= 3:
+        queue1.append(process)
+    elif priority >= 4 and priority <= 6:
+        queue2.append(process)
+    else:
+        queue3.append(process)
+print(queue1) #printing all the processes with burst time and process id and priority for queue 1
+print(queue2)#printing all the processes with burst time and process id and priority for queue 2
+print(queue3)#printing all the processes with burst time and process id and priority for queue 3
