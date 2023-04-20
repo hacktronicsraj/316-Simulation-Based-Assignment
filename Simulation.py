75525f5bae6d3da29489b0b3650c31d62fc6651a
hacktronicsraj <69853022+hacktronicsraj@users.noreply.github.com>
2023-04-20 14:44:18 +0530

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,12 +1,13 @@
+import time
 
 queue1 = []  # high priority range (0-3) for Round Robin t q 4
 queue2 = []  # medium priority range (4-6) for priority scheduling algorithm
 queue3 = []  # lowest priority range (7-9) for First come first serve algorithm
- 
+
 # Taking user input from the user of process id and priority and burst time
-num_processes = int(input("Enter the num of processes ,: "))
+num_processes = int(input("Enter the num of processes: "))
 for i in range(num_processes):
-    priority = int(input("Enter the priority of process {}, here the processes are divided into 3 queues as per their priority: ".format(i+1)))
+    priority = int(input("Enter the priority of process {}: ".format(i+1)))
     burst_time = int(input("Enter the burst time of process {}: ".format(i+1)))
     process = [i+1, priority, burst_time]  # Process id , and priority and birst time are stored in this order.
     if priority >= 0 and priority <= 3:
@@ -15,33 +16,25 @@ for i in range(num_processes):
         queue2.append(process)
     else:
         queue3.append(process)
-      
-queue1 = []
-queue2 = []
-queue3 = []
-#setting tq for each queue i.e 10
-quantum_time = 10
- 
-#there is no certain priority so we are taking queue1 as the current/starting queue
-current_queue = queue1
- 
-# Loop until all processes are executed
-total_time = 0
+
+total_time = 0  # total time taken to execute all processes
+current_queue = queue1  # starting queue is queue1
+quantum_time = 4  # time quantum for Round Robin algorithm
+
 while queue1 or queue2 or queue3:
-    # Apply Round Robin time quantum 4 for Queue 1
+    # execute processes in the current queue
     if current_queue == queue1:
         if queue1:
             process = queue1.pop(0)
             print("Executing process", process[0], "from Queue 1")
-            if process[2] > 4:
-                process[2] -= 4
-                total_time += 4
+            if process[2] > quantum_time:
+                process[2] -= quantum_time
+                total_time += quantum_time
                 queue2.append(process)
                 print("Process", process[0], "moved to Queue 2")
             else:
                 total_time += process[2]
                 print("Process", process[0], "finished execution")
-    # Apply priority scheduling algorithm for Queue 2
     elif current_queue == queue2:
         if queue2:
             queue2.sort(key=lambda x: x[1])
@@ -49,15 +42,14 @@ while queue1 or queue2 or queue3:
             print("Executing process", process[0], "from Queue 2")
             total_time += process[2]
             print("Process", process[0], "finished execution")
-    # Apply First come first serve algorithm for Queue 3
     elif current_queue == queue3:
         if queue3:
             process = queue3.pop(0)
             print("Executing process", process[0], "from Queue 3")
             total_time += process[2]
             print("Process", process[0], "finished execution")
- 
-    # Shift the CPU to the next queue after 10 seconds as it is roung robin for priority scheduling algo
+
+    # switch to the next queue every 10 seconds
     if total_time % 10 == 0:
         if current_queue == queue1:
             current_queue = queue2
@@ -65,6 +57,6 @@ while queue1 or queue2 or queue3:
             current_queue = queue3
         else:
             current_queue = queue1
- 
-# Print the total time taken to execute all processes
+
+# print the total time taken to execute all processes
 print("Total time taken to execute all processes:", total_time)
